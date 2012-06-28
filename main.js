7dc51bd8934c7d1b7087791f4990a77e76b175ca
Chris <chris@Damien.local>
2012-06-28 13:53:04 +0100

    updates

--- main.js
+++ main.js
@@ -5,11 +5,14 @@
   $ = jQuery;
 
   callback = function(data) {
-    return $(data).each(function(element) {
-      return alert(element.title);
+    $(document).append('<ol id="list"></ol>');
+    return $(data).each(function(i, element) {
+      return $('#list').append('<li>' + element.title + ' - ' + element.points + '</li>');
     });
   };
 
-  $.get('stories.json', null, callback, 'json');
+  $(document).ready(function() {
+    return $.get('stories.json', null, callback, 'json');
+  });
 
 }).call(this);
